a5dc93430ce67b30aafc660e1d3e0064b0cde1bc
Ingmar Ross <ingmar@perfectline.co>
2017-07-11 13:55:56 +0300

    Testimonials to foundation

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -12,6 +12,29 @@ $(document).ready(function(){
       lazyLoad: 'ondemand'
     });
   });
+  $('.testimonial-slider').each(function() {
+    $(this).slick({
+      prevArrow: '<div class="slick-prev"></div>',
+      nextArrow: '<div class="slick-next"></div>',
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
 
   setImageType();
   slideVideoResize();
