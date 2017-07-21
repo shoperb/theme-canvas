ff9b25e3f8a78d24c8bbc62bd2262dcc6cb89be2
Ingmar Ross <ingmar@perfectline.co>
2017-07-21 15:40:36 +0300

    Set settings values to integer where needed

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.slideshow').each(function() {
     var autoPlay = $(this).data('autoplay'),
         animationSpeed = $(this).data('animation-speed');
